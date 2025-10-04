76248fd69f6e71156976ccc864a0f60f20632080
CuzFriday <laqwchaos@gmail.com>
2025-10-04 09:31:12 -0500

    Revamp

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Function to scroll to a specific section
-function scrollToSection(index) {
-  const scrollContainer = document.getElementById("scrollContainer");
-  const sectionWidth = scrollContainer.scrollWidth / 6; // 6 sections
 
-  scrollContainer.scrollTo({
-    left: sectionWidth * index,
-    behavior: "smooth",
-  });
-}
